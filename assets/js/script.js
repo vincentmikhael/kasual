5fb9bced057bac9962584eb187a42b4d461e40e0
vincentmikhael <vincentmikhael24@gmail.com>
2021-01-28 13:20:12 +0700

    cart

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,21 @@
   $('select').on('change',function(e){
     $(event.target).addClass("red-color");
   })
+
+
+  	$(document).ready(function() {
+			$('.minus').click(function () {
+				var $input = $(this).parent().find('input');
+				var count = parseInt($input.val()) - 1;
+				count = count < 1 ? 1 : count;
+				$input.val(count);
+				$input.change();
+				return false;
+			});
+			$('.plus').click(function () {
+				var $input = $(this).parent().find('input');
+				$input.val(parseInt($input.val()) + 1);
+				$input.change();
+				return false;
+			});
+		});
